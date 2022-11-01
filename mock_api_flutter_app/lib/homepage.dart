c087b84092cbfdc2164906c60a9aac612af60d7c
Mohammad Mustafa <mustafa@sherpa-tech.com>
2022-11-01 22:23:50 +0545

    Detail page called

--- mock_api_flutter_app/lib/homepage.dart
+++ mock_api_flutter_app/lib/homepage.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
+import 'modal/film_modal.dart';
 import 'network/network_call.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,29 +24,58 @@ class _HomePageState extends State<HomePage> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasData) {
-            List data = snapshot.data as List;
-            return data.isNotEmpty?  ListView.separated(
-              itemCount: data.length,
-              separatorBuilder: (context, index) => SizedBox(
-                height: 10,
-              ),
-              itemBuilder: (ctx, index) {
-                return ListTile(
-                  tileColor: Colors.teal,
-                  title: Text(
-                    data[index]['title'],
-                  ),
-                  subtitle: Text(
-                    data[index]['director'],
-                  ),
-                );
-              },
-            ): Center(child: Text("Err"));
+            List<FilmModal> data = snapshot.data as List<FilmModal>;
+            return data.isNotEmpty
+                ? ListView.separated(
+                    itemCount: data.length,
+                    separatorBuilder: (context, index) => SizedBox(
+                      height: 10,
+                    ),
+                    itemBuilder: (ctx, index) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailPage(index: index + 1)));
+                        },
+                        child: ListTile(
+                          tileColor: Colors.teal,
+                          title: Text(data[index].title!),
+                          subtitle: Text(data[index].director!),
+                        ),
+                      );
+                    },
+                  )
+                : Center(
+                    child: Text("Err"),
+                  );
           } else {
             return Text("Error");
           }
         },
       ),
     );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  final int index;
+  const DetailPage({Key? key, required this.index}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  NetworkCall networkCall = NetworkCall();
+  @override
+  void initState() async {
+    final result = networkCall.getFilmDetail(widget.index);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
--- mock_api_flutter_app/lib/network/network_call.dart
+++ mock_api_flutter_app/lib/network/network_call.dart
@@ -3,18 +3,37 @@ import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 import 'package:mock_api_flutter_app/modal/film_modal.dart';
+
 class NetworkCall {
-  Future<List> getFilms() async{
-    final url = Uri.parse(BaseUrl+films);
-    final http.Response response  = await http.get(url);
-    if(response.statusCode == 200) {
-      final decodedResult = jsonDecode(response.body);
-      final List result =  decodedResult["results"];
-      return result;
+  Future<List> getFilms() async {
+    final url = Uri.parse(BaseUrl + films);
+
+    final http.Response response = await http.get(url);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedResult = jsonDecode(response.body);
+      final List result = decodedResult["results"];
+      List<FilmModal> listOfFilms =
+          result.map((e) => FilmModal.fromJson(e)).toList();
+      return listOfFilms;
     }
     return [];
+  }
+
+  Future<Map> getFilmDetail(int index) async {
+    final url = Uri.parse(BaseUrl + films + "/$index");
+
+    final http.Response response = await http.get(url);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedResult = jsonDecode(response.body);
+      print(decodedResult);
+      return decodedResult;
+
+    }
+    return {};
   }
 }
 
 const String BaseUrl = "https://swapi.dev/api/";
-const String films = "film";
+const String films = "films";
